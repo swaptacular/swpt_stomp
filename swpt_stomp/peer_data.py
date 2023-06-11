1b5e9e0c657801044a09860d287a8b3e36bd7578
Evgeni Pandurski <epandurski@gmail.com>
2023-06-11 16:37:30 +0300

    Work on peer_data.py

--- swpt_stomp/peer_data.py
+++ swpt_stomp/peer_data.py
@@ -1,9 +1,13 @@
+import re
 from enum import Enum
 from dataclasses import dataclass
 from typing import NamedTuple, Optional
 
+_ONLY_DIGITS_RE = re.compile(r"[0-9]+$")
+_DN_PART_RE = re.compile(r"(?!-)[a-z0-9-]{1,63}(?<!-)$", re.IGNORECASE)
 
-class PeerType(Enum):
+
+class NodeType(Enum):
     AA = 1  # Accounting Authority
     CA = 2  # Creditors Agent
     DA = 3  # Debtors Agent
@@ -12,6 +16,22 @@ class PeerType(Enum):
 class Subnet(NamedTuple):
     subnet: int
     subnet_mask: int
+
+    @classmethod
+    def parse(cls, s):
+        """Parse from a hexadecimal string."""
+        n = 4 * len(s)
+        if n > 64:
+            raise ValueError(f'invalid subnet: {s}')
+
+        try:
+            subnet = (int(s, base=16) << (64 - n)) if n > 0 else 0
+            if not 0 <= subnet < 0xffffffffffffffff:
+                raise ValueError
+        except ValueError:
+            raise ValueError(f'invalid subnet: {s}')
+
+        return Subnet(subnet, (-1 << (64 - n)) & 0xffffffffffffffff)
 
 
 @dataclass
@@ -25,7 +45,7 @@ class NodeData:
         'subnet',
     )
 
-    node_type: PeerType
+    node_type: NodeType
     node_id: str
     root_cert: bytes
     subnet: Optional[Subnet]
@@ -47,7 +67,7 @@ class PeerData:
         'subnet',
     )
 
-    node_type: PeerType
+    node_type: NodeType
     node_id: str
     servers: list[tuple[str, int]]
     stomp_host: Optional[str]
@@ -56,3 +76,54 @@ class PeerData:
     peer_cert: bytes
     sub_cert: Optional[bytes]
     subnet: Optional[Subnet]
+
+
+def _parse_node_type(s: str) -> NodeType:
+    if s == "Accounting Authorities":
+        return NodeType.AA
+    elif s == "Creditors Agents":
+        return NodeType.CA
+    elif s == "Debtors Agents":
+        return NodeType.DA
+    else:
+        raise ValueError(f'invalid node type: {s}')
+
+
+def _parse_servers(s: str) -> list[tuple[str, int]]:
+    servers = []
+    for server in s.split(maxsplit=10000):
+        try:
+            host, port_str = server.split(':', maxsplit=1)
+        except ValueError:
+            raise ValueError(f'invalid server: {s}')
+
+        if not _is_valid_hostname(host):
+            raise ValueError(f'invalid host: {host}')
+
+        try:
+            port = int(port_str)
+            if not 1 <= port <= 65535:
+                raise ValueError
+        except ValueError:
+            raise ValueError(f'invalid port: {port_str}')
+
+        servers.append((host, port))
+
+    return servers
+
+
+def _is_valid_hostname(hostname):
+    if hostname[-1] == ".":
+        # strip exactly one dot from the right, if present
+        hostname = hostname[:-1]
+
+    if len(hostname) > 253:
+        return False
+
+    labels = hostname.split(".")
+
+    # the TLD must be not all-numeric
+    if _ONLY_DIGITS_RE.match(labels[-1]):
+        return False
+
+    return all(_DN_PART_RE.match(label) for label in labels)
---
+++ tests/test_peer_data.py
@@ -0,0 +1,79 @@
+import pytest
+from swpt_stomp.peer_data import Subnet, NodeType
+
+
+def test_parse_subnet():
+    sn = Subnet.parse('0a')
+    assert sn.subnet == 0x0a00000000000000
+    assert sn.subnet_mask == 0xff00000000000000
+
+    sn = Subnet.parse('')
+    assert sn.subnet == 0x0000000000000000
+    assert sn.subnet_mask == 0x0000000000000000
+
+    sn = Subnet.parse('1234567890abcdef')
+    assert sn.subnet == 0x1234567890abcdef
+    assert sn.subnet_mask == 0xffffffffffffffff
+
+    with pytest.raises(ValueError):
+        Subnet.parse('-02')  # negative
+
+    with pytest.raises(ValueError):
+        Subnet.parse('0a0000000000000013431545')  # too long
+
+
+def test_parse_node_type():
+    from swpt_stomp.peer_data import _parse_node_type
+
+    assert _parse_node_type('Accounting Authorities') == NodeType.AA
+    assert _parse_node_type('Creditors Agents') == NodeType.CA
+    assert _parse_node_type('Debtors Agents') == NodeType.DA
+
+    with pytest.raises(ValueError):
+        _parse_node_type('something else')
+
+
+def test_parse_servers():
+    from swpt_stomp.peer_data import _parse_servers
+
+    assert _parse_servers('server1.example.com.:1234') == [
+        ('server1.example.com.', 1234)
+    ]
+
+    assert _parse_servers(
+        'server1.example.com:1234\n'
+        'server2.example.com:2345\n'
+    ) == [
+        ('server1.example.com', 1234),
+        ('server2.example.com', 2345),
+    ]
+
+    # Invalid port.
+    with pytest.raises(ValueError):
+        _parse_servers('server1.example.com:123456')
+
+    with pytest.raises(ValueError):
+        _parse_servers('server1.example.com:-1234')
+
+    with pytest.raises(ValueError):
+        _parse_servers('server1.example.com:0')
+
+    with pytest.raises(ValueError):
+        _parse_servers('server1.example.com:INVALID')
+
+    with pytest.raises(ValueError):
+        _parse_servers('server1.example.com:')
+
+    with pytest.raises(ValueError):
+        _parse_servers('server1.example.com')
+
+    # Invalid symbols in host.
+    with pytest.raises(ValueError):
+        _parse_servers('24[s]3q5:1234')
+
+    with pytest.raises(ValueError):
+        _parse_servers('example.999:1234')
+
+    # The host is too long.
+    with pytest.raises(ValueError):
+        _parse_servers(50 * 'abcdefgh.' + 'com:1234')
