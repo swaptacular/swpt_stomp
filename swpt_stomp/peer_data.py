f9957c49f238f87a5ab16fc9d4c547bd1c810d8a
Evgeni Pandurski <epandurski@gmail.com>
2023-06-12 16:31:12 +0300

    Work on peer_data.py

--- swpt_stomp/peer_data.py
+++ swpt_stomp/peer_data.py
@@ -2,18 +2,25 @@ import re
 import os
 import os.path
 import asyncio
+import time
 from concurrent.futures import ThreadPoolExecutor
+from collections import deque
 from functools import partial
 from abc import ABC, abstractmethod
 from enum import Enum
 from dataclasses import dataclass
 from typing import NamedTuple, Optional
 
-_DN_PART_RE = re.compile(r"(?!-)[a-z0-9-]{1,63}(?<!-)$", re.IGNORECASE)
+# The `EXECUTOR_THREADS` environment variable specifies the number of
+# threads for the default `ThreadPoolExecutor`.
 
-# The name of the environment variable that specifies the number of threads
-# for the default `ThreadPoolExecutor`.
-_EXECUTOR_THREADS_ENVVAR = 'EXECUTOR_THREADS'
+# The `MAX_CACHED_PEERS` environment variable specifies the maximum number
+# of cached peer data records.
+
+# The `PEERS_CACHE_SECONDS environment variable specifies the number seconds
+# during which cached peer data records will be considered fresh.
+
+_DN_PART_RE = re.compile(r"(?!-)[a-z0-9-]{1,63}(?<!-)$", re.IGNORECASE)
 
 
 class NodeType(Enum):
@@ -92,12 +99,66 @@ class DatabaseError(Exception):
 class NodePeersDatabase(ABC):
     """A database containing information for the node and its peers."""
 
-    @abstractmethod
+    def __init__(self) -> None:
+        self.__node_data: Optional[NodeData] = None
+        self.__peers_dict: dict[str, _PeerCacheRecord] = dict()
+        self.__peers_deque: deque[_PeerCacheRecord] = deque()
+        self.__max_cached_peers = int(
+            os.environ.get('MAX_CACHED_PEERS', '5000'))
+        self.__peers_cache_seconds = float(
+            os.environ.get('PEERS_CACHE_SECONDS', '600'))
+
+    def __get_stored_peer_data(self, node_id: str) -> Optional[PeerData]:
+        if peer_record := self.__peers_dict.get(node_id, None):
+            cutoff_time = time.time() - self.__peers_cache_seconds
+            if peer_record.time >= cutoff_time:
+                return peer_record.peer_data
+
+        return None
+
+    def __store_peer_data(self, peer_data: PeerData) -> None:
+        node_id = peer_data.node_id
+        peers_dict = self.__peers_dict
+        peers_deque = self.__peers_deque
+
+        new_record = _PeerCacheRecord(
+            peer_data=peer_data,
+            time=time.time(),
+        )
+        peers_dict[node_id] = new_record
+        peers_deque.append(new_record)
+        while len(peers_deque) > self.__max_cached_peers:
+            oldest_record = peers_deque.popleft()
+            oldest_record_node_id = oldest_record.peer_data.node_id
+            if oldest_record_node_id != node_id:
+                peers_dict.pop(oldest_record_node_id, None)
+
     async def get_node_data(self) -> NodeData:
+        """Return data for owner of the node."""
+
+        if self.__node_data is None:
+            self.__node_data = await self._get_node_data()
+
+        return self.__node_data
+
+    async def get_peer_data(self, node_id: str) -> Optional[PeerData]:
+        """Return data for peer with the given node ID."""
+
+        if peer_data := self.__get_stored_peer_data(node_id):
+            return peer_data
+
+        if peer_data := await self._get_peer_data(node_id):
+            self.__store_peer_data(peer_data)
+            return peer_data
+
+        return None
+
+    @abstractmethod
+    async def _get_peer_data(self, node_id: str) -> Optional[PeerData]:
         raise NotImplementedError  # pragma: nocover
 
     @abstractmethod
-    async def get_peer_data(self, node_id: str) -> Optional[PeerData]:
+    async def _get_node_data(self) -> NodeData:
         raise NotImplementedError  # pragma: nocover
 
 
@@ -117,33 +178,41 @@ def get_database_instance(url: str) -> NodePeersDatabase:
     raise ValueError(f'invalid database URL: {url}')
 
 
+@dataclass
+class _PeerCacheRecord:
+    slots = ('peer_data', 'time')
+    peer_data: PeerData
+    time: float
+
+
 class _LocalDirectory(NodePeersDatabase):
     def __init__(self, url: str):
+        super().__init__()
+
         assert url.startswith('file:///')
         self._root_dir: str = os.path.normpath(url[7:])
         self._loop = asyncio.get_event_loop()
-        self._node_data: Optional[NodeData] = None
 
-        n = str(5 * (os.cpu_count() or 1))
-        self._executor = ThreadPoolExecutor(
-            max_workers=int(os.environ.get(_EXECUTOR_THREADS_ENVVAR, n)))
+        default_threads = str(5 * (os.cpu_count() or 1))
+        max_workers = int(os.environ.get('EXECUTOR_THREADS', default_threads))
+        self._executor = ThreadPoolExecutor(max_workers=max_workers)
 
-    def _read_file(self, filepath: str) -> bytes:
+    def _fetch_file(self, filepath: str) -> bytes:
         abspath = os.path.join(self._root_dir, filepath)
         with open(abspath, 'br') as f:
             return f.read()
 
-    async def read_file(self, filepath: str) -> bytes:
+    async def _read_file(self, filepath: str) -> bytes:
         return await self._loop.run_in_executor(
             self._executor,
-            partial(self._read_file, filepath),
+            partial(self._fetch_file, filepath),
         )
 
-    async def read_pem_file(self, filepath: str) -> bytes:
-        return await self.read_file(filepath)
+    async def _read_pem_file(self, filepath: str) -> bytes:
+        return await self._read_file(filepath)
 
-    async def read_oneline(self, filepath: str) -> str:
-        content = await self.read_file(filepath)
+    async def _read_oneline(self, filepath: str) -> str:
+        content = await self._read_file(filepath)
         if content.endswith(b'\r\n'):
             content = content[:-2]
         elif content.endswith(b'\n'):
@@ -154,9 +223,9 @@ class _LocalDirectory(NodePeersDatabase):
         except UnicodeDecodeError as e:
             raise DatabaseError from e
 
-    async def read_subnet_file(self, filepath: str) -> Optional[Subnet]:
+    async def _read_subnet_file(self, filepath: str) -> Optional[Subnet]:
         try:
-            s = await self.read_oneline(filepath)
+            s = await self._read_oneline(filepath)
         except FileNotFoundError:
             return None
 
@@ -165,32 +234,29 @@ class _LocalDirectory(NodePeersDatabase):
         except ValueError as e:
             raise DatabaseError from e
 
-    async def get_node_data(self) -> NodeData:
-        if self._node_data is None:
-            root_cert = await self.read_pem_file('root-ca.crt')
-            node_id = await self.read_oneline('db/nodeid')
-            node_type_str = await self.read_oneline('db/nodetype')
-            try:
-                node_type = _parse_node_type(node_type_str)
-            except ValueError as e:
-                raise DatabaseError from e
+    async def _get_node_data(self) -> NodeData:
+        root_cert = await self._read_pem_file('root-ca.crt')
+        node_id = await self._read_oneline('db/nodeid')
+        node_type_str = await self._read_oneline('db/nodetype')
+        try:
+            node_type = _parse_node_type(node_type_str)
+        except ValueError as e:
+            raise DatabaseError from e
 
-            subnet = None
-            if node_type == NodeType.CA:
-                subnet = await self.read_subnet_file('creditors-subnet.txt')
-            elif node_type == NodeType.DA:
-                subnet = await self.read_subnet_file('debtors-subnet.txt')
+        subnet = None
+        if node_type == NodeType.CA:
+            subnet = await self._read_subnet_file('creditors-subnet.txt')
+        elif node_type == NodeType.DA:
+            subnet = await self._read_subnet_file('debtors-subnet.txt')
 
-            self._node_data = NodeData(
-                node_type=node_type,
-                node_id=node_id,
-                root_cert=root_cert,
-                subnet=subnet,
-            )
+        return NodeData(
+            node_type=node_type,
+            node_id=node_id,
+            root_cert=root_cert,
+            subnet=subnet,
+        )
 
-        return self._node_data
-
-    async def get_peer_data(self, node_id: str) -> Optional[PeerData]:
+    async def _get_peer_data(self, node_id: str) -> Optional[PeerData]:
         raise NotImplementedError
 
 
