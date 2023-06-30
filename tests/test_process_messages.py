f252d677e9a39abdc2ff891d645c1cf68081c565
Evgeni Pandurski <epandurski@gmail.com>
2023-06-30 15:46:13 +0300

    Add tests

--- tests/test_process_messages.py
+++ tests/test_process_messages.py
@@ -1,9 +1,40 @@
 import pytest
+import json
 from swpt_stomp.common import Message
 from swpt_stomp.rmq import RmqMessage
+from swpt_stomp.peer_data import Subnet, get_database_instance
 from swpt_stomp.process_messages import (
     ProcessingError, transform_message, preprocess_message,
 )
+
+
+def create_account_purge_msg(debtor_id: int, creditor_id: int) -> str:
+    props = f"""
+      "type": "AccountPurge",
+      "debtor_id": {debtor_id},
+      "creditor_id": {creditor_id},
+      "creation_date": "2001-01-01",
+      "ts": "2023-01-01T12:00:00+00:00"
+    """
+    return '{' + props + '}'
+
+
+def create_prepare_transfer_msg(debtor_id: int, creditor_id: int) -> str:
+    props = f"""
+      "type": "PrepareTransfer",
+      "debtor_id": {debtor_id},
+      "creditor_id": {creditor_id},
+      "min_locked_amount": 1000,
+      "max_locked_amount": 2000,
+      "recipient": "RECIPIENT",
+      "min_interest_rate": -10.0,
+      "max_commit_delay": 100000,
+      "coordinator_type": "test",
+      "coordinator_id": 789,
+      "coordinator_request_id": 1111,
+      "ts": "2023-01-01T12:00:00+00:00"
+    """
+    return '{' + props + '}'
 
 
 def test_calc_bin_routing_key():
@@ -27,7 +58,6 @@ def test_as_hex():
 
 
 def test_change_subnet():
-    from swpt_stomp.peer_data import Subnet
     from swpt_stomp.process_messages import _change_subnet
 
     assert _change_subnet(
@@ -54,28 +84,10 @@ def test_change_subnet():
 def test_parse_message_body():
     from swpt_stomp.process_messages import _parse_message_body
 
-    acc_purge_body = bytearray("""{
-      "type": "AccountPurge",
-      "debtor_id": 123,
-      "creditor_id": 456,
-      "creation_date": "2001-01-01",
-      "ts": "2023-01-01T12:00:00Z"
-    }""".encode('utf8'))
-
-    prep_transfer_body = bytearray("""{
-      "type": "PrepareTransfer",
-      "debtor_id": 123,
-      "creditor_id": 456,
-      "min_locked_amount": 1000,
-      "max_locked_amount": 2000,
-      "recipient": "RECIPIENT",
-      "min_interest_rate": -10.0,
-      "max_commit_delay": 100000,
-      "coordinator_type": "test",
-      "coordinator_id": 789,
-      "coordinator_request_id": 1111,
-      "ts": "2023-01-01T12:00:00Z"
-    }""".encode('utf8'))
+    acc_purge_body = bytearray(
+        create_account_purge_msg(123, 456).encode('utf8'))
+    prep_transfer_body = bytearray(
+        create_prepare_transfer_msg(123, 456).encode('utf8'))
 
     with pytest.raises(ProcessingError):
         _parse_message_body(Message(
@@ -179,3 +191,96 @@ def test_parse_message_body():
     assert obj["creditor_id"] == 456
     assert obj["coordinator_type"] == "test"
     assert obj["coordinator_id"] == 789
+
+
+@pytest.mark.asyncio
+async def test_transform_message_aa(datadir):
+    db = get_database_instance(url=f'file://{datadir["AA"]}')
+    owner_node_data = await db.get_node_data()
+
+    def transform(s: str) -> Message:
+        message = RmqMessage(
+            id='1',
+            type='AccountPurge',
+            body=s.encode('utf8'),
+            content_type='application/json',
+            headers={},
+            routing_key=None,
+        )
+        return transform_message(owner_node_data, peer_data, message)
+
+    # Test sending AccountPurge messages to CA:
+    peer_data = await db.get_peer_data('5921983fe0e6eb987aeedca54ad3c708')
+    s = create_account_purge_msg(0x1234abcd00000001, 0x0000010000000abc)
+    m = transform(s)
+    assert isinstance(m, Message)
+    assert m.id == '1'
+    assert m.type == 'AccountPurge'
+    assert m.content_type == 'application/json'
+    assert json.loads(m.body.decode('utf8')) == json.loads(s)
+
+    s = create_account_purge_msg(0x1234abce00000001, 0x0000010000000abc)
+    with pytest.raises(ProcessingError):
+        # invalid debtor ID
+        transform(s)
+
+    s = create_account_purge_msg(0x1234abcd00000001, 0x0000020000000abc)
+    with pytest.raises(ProcessingError):
+        # invalid creditor ID
+        transform(s)
+
+    # Test sending AccountPurge messages to DA:
+    peer_data = await db.get_peer_data('060791aeca7637fa3357dfc0299fb4c5')
+    s = create_account_purge_msg(0x1234abcd00000001, 0x0000000000000000)
+    m = transform(s)
+    assert isinstance(m, Message)
+    assert m.id == '1'
+    assert m.type == 'AccountPurge'
+    assert m.content_type == 'application/json'
+    assert json.loads(m.body.decode('utf8')) == json.loads(s)
+
+    s = create_account_purge_msg(0x1234abce01000001, 0x0000000000000000)
+    with pytest.raises(ProcessingError):
+        # invalid debtor ID
+        transform(s)
+
+    s = create_account_purge_msg(0x1234abcd00000001, 0x0000000000000001)
+    with pytest.raises(ProcessingError):
+        # invalid creditor ID
+        transform(s)
+
+
+@pytest.mark.asyncio
+async def test_transform_message_ca(datadir):
+    db = get_database_instance(url=f'file://{datadir["CA"]}')
+    owner_node_data = await db.get_node_data()
+    peer_data = await db.get_peer_data('1234abcd')
+
+    def transform(s: str) -> Message:
+        message = RmqMessage(
+            id='1',
+            type='PrepareTransfer',
+            body=s.encode('utf8'),
+            content_type='application/json',
+            headers={},
+            routing_key=None,
+        )
+        return transform_message(owner_node_data, peer_data, message)
+
+    s = create_prepare_transfer_msg(0x1234abcd00000001, 0x0000010000000abc)
+    m = transform(s)
+    assert isinstance(m, Message)
+    assert m.id == '1'
+    assert m.type == 'PrepareTransfer'
+    assert m.content_type == 'application/json'
+    assert json.loads(m.body.decode('utf8')) == json.loads(s)
+
+    # Invalid creditor ID:
+    s = create_prepare_transfer_msg(0x1234abcd00000001, 0x0000020000000abc)
+    with pytest.raises(ProcessingError):
+        transform(s)
+
+    # Invalid debtor ID:
+    s = create_prepare_transfer_msg(0x1234abce00000001, 0x0000010000000abc)
+    with pytest.raises(ProcessingError):
+        transform(s)
